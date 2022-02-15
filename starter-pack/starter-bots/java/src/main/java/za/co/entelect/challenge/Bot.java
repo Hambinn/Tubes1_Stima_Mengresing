b627372ad392507e5b411da8d1c93712f59800c0
Hambinn <ilhambintangn@gmail.com>
2022-02-15 22:19:35 +0700

    benerin lagi

--- starter-pack/starter-bots/java/src/main/java/za/co/entelect/challenge/Bot.java
+++ starter-pack/starter-bots/java/src/main/java/za/co/entelect/challenge/Bot.java
@@ -109,7 +109,7 @@ public class Bot {
         List<Object> blocks = new ArrayList<>();
         int startBlock = map.get(0)[0].position.block;
 
-        Lane[] laneList = map.get(lane);
+        Lane[] laneList = map.get(lane - 1);
         for (int i = max(block - startBlock, 0); i <= block - startBlock + speed; i++) {
             if (laneList[i] == null || laneList[i].terrain == Terrain.FINISH) {
                 break;
